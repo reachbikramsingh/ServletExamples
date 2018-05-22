5384a3464969b72ffc7f8356c0931c8b52eaaf8d
reachbikramsingh <reachbikramsingh@gmail.com>
2018-05-22 15:51:13 +0530

    RequestDispatcherExample-first

---
+++ src/web/Welcome.java
@@ -0,0 +1,26 @@
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Welcome")
+public class Welcome extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		try {
+			out.println("<h2>Welcome user here</h2>");
+		} finally {
+			out.close();
+		}
+	}
+}
